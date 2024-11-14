fe8ed107837219d504fc50bc4ce51f0571ddd111
Marcelo554 <nierosistemas@gmail.com>
2024-11-14 18:21:25 -0300

    alterações criando botões Novo/Alterar

--- routes/usuario/forms.py
+++ routes/usuario/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 class Class_Form_Cadastro_Usuario(FlaskForm):
-    idusuario = IntegerField('idusuario')
+    idusuario = IntegerField('idusuario', validators=[Optional()])  # Optional permite valores em branco
     nome = StringField('Nome', validators=[DataRequired()])
     telefone = StringField('Telefone')
-    senha = PasswordField('Senha', validators=[DataRequired()])
+    senha = PasswordField('Senha')
     submit = SubmitField('Enviar')
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.idusuario.data = ""  # Define o valor inicial como string vazia
+    #     self.idusuario.nome = ""  # Define o valor inicial como string vazia
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
--- routes/usuario/rotas_usuario.py
+++ routes/usuario/rotas_usuario.py
@@ -3,10 +3,9 @@
 """
 
 # Ajuste o import conforme necessário
-from .forms import Class_Form_Cadastro_Usuario
 from flask import flash, request, render_template, send_file
 import logging
-from flask import Blueprint,  redirect, url_for, jsonify
+from flask import Blueprint, redirect, url_for, jsonify
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 from models.models import Usuario, db
@@ -19,25 +18,24 @@ from fpdf import FPDF
 from io import BytesIO
 
 
-
-
 # Define o Blueprint para o módulo de usuário
-usuario_blueprint = Blueprint('usuario', __name__, template_folder='templates')
+usuario_blueprint = Blueprint("usuario", __name__, template_folder="templates")
 
-@usuario_blueprint.route('/inserir', methods=['GET', 'POST'])
+
+@usuario_blueprint.route("/inserir", methods=["GET", "POST"])
 def inserir():
     """
     Inserir usuario *** ROTA VALIDADA ***
     Template = usuario_incluir.html
     """
 
-    print('============> ENTREI NO FORMULARIO')
+    print("============> ENTREI NO FORMULARIO")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # Pega os dados do formulário
-        codigo = request.form.get('codigo', '').strip()
-        nome = request.form.get('nome', '').strip().upper()
-        telefone = request.form.get('telefone','')
+        codigo = request.form.get("codigo", "").strip()
+        nome = request.form.get("nome", "").strip().upper()
+        telefone = request.form.get("telefone", "")
 
         # Verifique se os dados foram recebidos corretamente
         print(f"Código recebido: {codigo}, Nome recebido: {nome}")
@@ -48,33 +46,31 @@ def inserir():
             # Criar uma nova instância do usuário
             novo_usuario = Usuario(codigo=codigo, nome=nome)
 
-            print('Tentando inserir usuário no banco de dados...')
+            print("Tentando inserir usuário no banco de dados...")
 
             db.session.add(novo_usuario)
 
-            print('Usuário adicionado à sessão. Tentando commit...')
+            print("Usuário adicionado à sessão. Tentando commit...")
 
             db.session.commit()  # Isso deve efetivar a inserção no banco de dados
 
             print(novo_usuario.codigo)
             print(novo_usuario.nome)
 
-            print('Usuário inserido com sucesso!')
+            print("Usuário inserido com sucesso!")
 
             # Redireciona para o mesmo formulário após sucesso
-            return redirect(url_for('usuario.inserir'))
+            return redirect(url_for("usuario.inserir"))
 
         except SQLAlchemyError as e:
             # Em caso de erro, faz rollback
             # Imprime a mensagem de erro
-            print('Falha ao inserir o usuário!', str(e))
+            print("Falha ao inserir o usuário!", str(e))
             db.session.rollback()
-            flash(f'Erro ao inserir dados: {str(e)}', 'error')
+            flash(f"Erro ao inserir dados: {str(e)}", "error")
 
     # Se for uma requisição GET, exibe o formulário para cadastrar novo usuário
-    return render_template('usuario_incluir.html')
-
-
+    return render_template("usuario_incluir.html")
 
 
 @usuario_blueprint.route("/lista2")
@@ -87,32 +83,30 @@ def lista2():
     usuarios = Usuario.query.order_by(Usuario.nome.asc()).all()
 
     # Passe 'usuarios' para o template
-    return render_template('usuariolista2.html', usuarios=usuarios)
+    return render_template("usuariolista2.html", usuarios=usuarios)
 
 
-
-@usuario_blueprint.route('/buscar_usuario_por_codigo', methods=['GET', 'POST'])
+@usuario_blueprint.route("/buscar_usuario_por_codigo", methods=["GET", "POST"])
 def buscar_usuario_por_codigo(codigo):
     # Tenta buscar o usuário no banco de dados pelo código fornecido
     usuario = Usuario.query.filter_by(codigo=codigo).first()
     return usuario
 
 
-@usuario_blueprint.route("/consultar_codigo", methods=['GET', 'POST'])
+@usuario_blueprint.route("/consultar_codigo", methods=["GET", "POST"])
 def consultar_codigo():
     form = Class_Form_Cadastro_Usuario()  # Instancie o formulário
-    if request.method == 'POST':
+    if request.method == "POST":
         codigo = form.codigo.data  # Obtenha o valor do campo codigo do formulário
         usuario = buscar_usuario_por_codigo(codigo)  # Sua função para buscar o usuário
 
         if usuario:
-            return render_template('seu_template.html', usuario=usuario, form=form)
+            return render_template("seu_template.html", usuario=usuario, form=form)
         else:
-            flash('Usuário não encontrado!', 'danger')
+            flash("Usuário não encontrado!", "danger")
 
     # Retorne o formulário caso seja um GET ou não encontre o usuário
-    return render_template('usuario_manutencao_cadastro.html', form=form)
-
+    return render_template("usuario_manutencao_cadastro.html", form=form)
 
 
 """
@@ -122,9 +116,9 @@ def consultar_codigo():
 
 @usuario_blueprint.route("/rota_exibe_grid_usuarios_cadastrados")
 def rota_exibe_grid_usuarios_cadastrados():
-    """ Rota que exibe na grade os usuarios cadastrados """
+    """Rota que exibe na grade os usuarios cadastrados"""
     form = Class_Form_Cadastro_Usuario()
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     per_page = 10  # Número de itens por página
 
     # Pegue os primeiros 'per_page' itens para a página atual
@@ -139,15 +133,14 @@ def rota_exibe_grid_usuarios_cadastrados():
         form=form,
         result=result,  # Passa os itens da página
         total=total_cadastrado,
-        usuarios=result  # Passando o objeto para o template
+        usuarios=result,  # Passando o objeto para o template
     )
-
 
 
 @usuario_blueprint.route("/rota_paginacao_dados_usuarios_cadastrados")
 def rota_paginacao_dados_usuarios_cadastrados():
     form = Class_Form_Cadastro_Usuario()
-    page = request.args.get('page', 1, type=int)  # Página atual
+    page = request.args.get("page", 1, type=int)  # Página atual
     per_page = 10  # Número de itens por página
 
     # Obtendo todos os usuários de uma vez
@@ -169,77 +162,62 @@ def rota_paginacao_dados_usuarios_cadastrados():
         "usuario_tela_base.html",
         form=form,
         result=result,  # Passando os itens da página
-        total=total,     # Total de registros
-        page=page,       # Página atual
+        total=total,  # Total de registros
+        page=page,  # Página atual
         per_page=per_page,  # Itens por página
         total_pages=total_pages,  # Total de páginas
     )
 
 
 
+
+
 @usuario_blueprint.route("/rota_inclusao_manutencao_cadastro_usuario", methods=["GET", "POST"])
 def rota_inclusao_manutencao_cadastro_usuario():
-    """ INCLUSÃO / ALTERAÇÃO """
-    print('Entrei aqui 1 ... ')
     form = Class_Form_Cadastro_Usuario()
+    acao = request.form.get("acao")  # Captura o valor da ação
 
-    # VALIDANDO O FORMULÁRIO PARA GRAVAÇÃO DOS DADOS
-    form.validate()
-    if form.validate_on_submit():
-        # Se a validação passar, o código aqui será executado
-        print("Formulário válido!")
-        # Aqui você faria o que quiser com os dados do formulário, como salvar no banco de dados
-    else:
-        print("Formulário inválido!")
-        # Imprimir os erros para o usuário
-        for field, errors in form.errors.items():
-            for error in errors:
-                print(f"Erro em {field}: {error}")
-                flash(f"Erro em {field}: {error}")
+    print('Ação=', acao)
 
-
-    # GRAVANDO OS DADOS NA TABELA (INSERT/UPDATE)
-    if form.validate_on_submit():
-        print('Entrei aqui 2 ... ')
-        # Obtendo dados do formulário
+    if request.method == "POST" and form.validate_on_submit():
         idusuario = form.idusuario.data
         nome = form.nome.data
         telefone = form.telefone.data
         senha = form.senha.data
 
-        print('idusuario = ')
-        print(idusuario)
+        try:
+            if acao == "alterar" and idusuario:  # Para alteração de registro
+                usuario_existente = Usuario.query.get(idusuario)
+                if usuario_existente:
+                    usuario_existente.nome = nome.upper()
+                    usuario_existente.telefone = telefone
+                    usuario_existente.senha = senha
+                    db.session.commit()
+                    flash("Usuário atualizado com sucesso!", "success")
+                else:
+                    flash("Usuário não encontrado.", "warning")
+            elif acao == "novo":  # Para inclusão de novo registro
+                novo_usuario = Usuario(nome=nome.upper(), telefone=telefone, senha=senha)
+                db.session.add(novo_usuario)
+                db.session.commit()
+                flash("Usuário cadastrado com sucesso!", "success")
+            else:
+                flash("Ação inválida ou ID de usuário não fornecido para alteração.", "error")
+        except Exception as e:
+            db.session.rollback()
+            flash("Ocorreu um erro ao salvar o usuário.", "error")
+            print(f"Erro ao salvar usuário: {e}")
 
-        # Verificando se o usuário já existe
-        usuario_existente = Usuario.query.filter_by(idusuario=idusuario).first()
-
-        print(usuario_existente)
-
-        if usuario_existente:
-            print('Entrei aqui 4 ... ')
-            # Atualizando o usuário existente
-            usuario_existente.idusuario = idusuario
-            usuario_existente.nome = nome.upper()
-            usuario_existente.telefone = telefone
-            usuario_existente.senha = senha
-            db.session.commit()
-            flash('Usuário atualizado com sucesso!', 'success')
-        else:
-            print('Entrei aqui 5 ... ')
-            # Criando novo usuário
-            novo_usuario = Usuario(idusuario=idusuario, nome=nome.upper(), telefone=telefone, senha=senha)
-            db.session.add(novo_usuario)
-            db.session.commit()
-            flash('Usuário cadastrado com sucesso!', 'success')
-
-        print('Entrei aqui 3 ... ')
         return redirect(url_for('usuario.rota_inclusao_manutencao_cadastro_usuario'))
+
+    # Código restante para GET e renderização de template
+
 
     # Obtendo todos os usuários ordenados pelo nome
     usuarios = Usuario.query.order_by(Usuario.nome).all()
 
-    # Paginação
-    page = request.args.get('page', 1, type=int)
+    # Paginação ---------------------------------------------------------
+    page = request.args.get("page", 1, type=int)
     per_page = 10
     total = len(usuarios)
     start = (page - 1) * per_page
@@ -255,16 +233,18 @@ def rota_inclusao_manutencao_cadastro_usuario():
         page=page,
         per_page=per_page,
         total_pages=total_pages,
-        pessoa=[{'codigo': '', 'Nome': '', 'senha': ''}]  # Inicializando o formulário de novo cadastro vazio
+        pessoa=[
+            {"codigo": "", "Nome": "", "senha": ""}
+        ],  # Inicializando o formulário de novo cadastro vazio
     )
 
 
-@usuario_blueprint.route('/buscar_usuario', methods=['GET'])
+@usuario_blueprint.route("/buscar_usuario", methods=["GET"])
 def buscar_usuario():
     """Busca idusuario no banco"""
-    print('Oiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa !!!!!!!!!')
-    print('Entrei na rotina buscar_usuario ......')
-    id_usuario = request.args.get('idusuario')
+    print("Oiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa !!!!!!!!!")
+    print("Entrei na rotina buscar_usuario ......")
+    id_usuario = request.args.get("idusuario")
     print(f"Recebido idusuario: {id_usuario}")
 
     try:
@@ -275,26 +255,28 @@ def buscar_usuario():
     usuario = Usuario.query.filter_by(idusuario=id_usuario).first()
     if usuario:
         print(f"Usuário encontrado: {usuario.nome}")
-        return jsonify({
-            'idusuario': usuario.idusuario,
-            'nome': usuario.nome,
-            'telefone': usuario.telefone,
-            'senha': usuario.senha
-        })
+        return jsonify(
+            {
+                "idusuario": usuario.idusuario,
+                "nome": usuario.nome,
+                "telefone": usuario.telefone,
+                "senha": usuario.senha,
+            }
+        )
     else:
         print("Usuário não encontrado.")
         return jsonify({"error": "Usuário não encontrado"}), 404
 
 
-@usuario_blueprint.route('/deletar', methods=['POST'])
+@usuario_blueprint.route("/deletar", methods=["POST"])
 def deletar_usuario():
     """Deleta um usuário do banco"""
     data = request.get_json()
-    if not data or 'idusuario' not in data:
+    if not data or "idusuario" not in data:
         print("Dados inválidos recebidos.")
         return jsonify({"error": "Dados inválidos"}), 400
 
-    id_usuario = data['idusuario']
+    id_usuario = data["idusuario"]
     print(f"Tentando deletar idusuario: {id_usuario}")
 
     try:
@@ -314,44 +296,56 @@ def deletar_usuario():
         return jsonify({"error": "Usuário não encontrado"}), 404
 
 
-
 class PDF(FPDF):
-    """ Classe FPDF que formata o relatorio"""
+    """Classe FPDF que formata o relatorio"""
+
     def header(self):
-        self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, 'Relatório de Usuários', 0, 1, 'C')
-        self.set_font('Arial', 'I', 10)
+        self.set_font("Arial", "B", 12)
+        self.cell(0, 10, "Relatório de Usuários", 0, 1, "C")
+        self.set_font("Arial", "I", 10)
         self.ln(10)
 
     def footer(self):
         self.set_y(-15)
-        self.set_font('Arial', 'I', 8)
+        self.set_font("Arial", "I", 8)
         current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        self.cell(0, 10, f'Emissão: {current_time}', 0, 0, 'L')
-        self.cell(0, 10, f'Página {self.page_no()}', 0, 0, 'R')
+        self.cell(0, 10, f"Emissão: {current_time}", 0, 0, "L")
+        self.cell(0, 10, f"Página {self.page_no()}", 0, 0, "R")
 
     def chapter_title(self, title):
-        self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, title, 0, 1, 'C')
+        self.set_font("Arial", "B", 12)
+        self.cell(0, 10, title, 0, 1, "C")
         self.ln(10)
 
     def chapter_body(self, body):
-        self.set_font('Arial', '', 10)
+        self.set_font("Arial", "", 10)
         self.multi_cell(0, 10, body)
 
 
-@usuario_blueprint.route('/filtrar_usuario', methods=['GET'])
+@usuario_blueprint.route("/filtrar_usuario", methods=["GET"])
 def filtrar_usuario():
-    nome = request.args.get('nome', '').upper()  # Obtém o nome do parâmetro da requisição e converte para maiúsculo
-    usuarios = Usuario.query.filter(Usuario.nome.like(f'%{nome}%')).all()  # Filtra usuários pelo nome
+    nome = request.args.get(
+        "nome", ""
+    ).upper()  # Obtém o nome do parâmetro da requisição e converte para maiúsculo
+    usuarios = Usuario.query.filter(
+        Usuario.nome.like(f"%{nome}%")
+    ).all()  # Filtra usuários pelo nome
 
     # Formata os resultados como uma lista de dicionários
-    result = [{'idusuario': usuario.idusuario, 'codigo': usuario.codigo, 'nome': usuario.nome, 'senha': usuario.senha} for usuario in usuarios]
+    result = [
+        {
+            "idusuario": usuario.idusuario,
+            "codigo": usuario.codigo,
+            "nome": usuario.nome,
+            "senha": usuario.senha,
+        }
+        for usuario in usuarios
+    ]
 
-    return jsonify({'result': result})
+    return jsonify({"result": result})
 
 
-@usuario_blueprint.route('/imprimir_cadastro', methods=['GET'])
+@usuario_blueprint.route("/imprimir_cadastro", methods=["GET"])
 def imprimir_cadastro():
     usuarios = Usuario.query.order_by(Usuario.nome).all()
 
@@ -359,14 +353,14 @@ def imprimir_cadastro():
     pdf.add_page()
 
     # Definir cabeçalhos da tabela
-    pdf.set_font('Arial', 'B', 10)
-    pdf.cell(10, 8, 'ID', 1)
-    pdf.cell(80, 8, 'Nome', 1)
-    pdf.cell(60, 8, 'Telefone', 1)
+    pdf.set_font("Arial", "B", 10)
+    pdf.cell(10, 8, "ID", 1)
+    pdf.cell(80, 8, "Nome", 1)
+    pdf.cell(60, 8, "Telefone", 1)
     pdf.ln()
 
     # Adicionar dados dos usuários
-    pdf.set_font('Arial', '', 8)
+    pdf.set_font("Arial", "", 8)
     total_usuarios = 0
     for usuario in usuarios:
         pdf.cell(10, 10, str(usuario.idusuario), 1)
@@ -379,11 +373,11 @@ def imprimir_cadastro():
 
     # Adicionar total de registros
     pdf.ln(10)
-    pdf.set_font('Arial', 'B', 12)
-    pdf.cell(0, 10, f'Total de Registros: {total_usuarios}', 0, 1, 'L')
+    pdf.set_font("Arial", "B", 12)
+    pdf.cell(0, 10, f"Total de Registros: {total_usuarios}", 0, 1, "L")
 
     # Gera o PDF e obtém o conteúdo como string
-    pdf_content = pdf.output(dest='S').encode('latin1')
+    pdf_content = pdf.output(dest="S").encode("latin1")
 
     # Cria um objeto BytesIO e escreve o conteúdo do PDF nele
     buffer = BytesIO()
@@ -391,4 +385,6 @@ def imprimir_cadastro():
     buffer.seek(0)
 
     # ABRIR O ARQUIVO GERADO
-    return send_file(buffer, mimetype='application/pdf', download_name='relatorio_usuarios.pdf')
+    return send_file(
+        buffer, mimetype="application/pdf", download_name="relatorio_usuarios.pdf"
+    )
